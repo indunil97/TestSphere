5a76356215903d208008d7d00331606acf72f1a4
LAPTOP-HMAUVDFR\sAc <indunil97@gmail.com>
2026-03-27 23:19:04 +0530

    feat: add delete request API

--- backend/src/controllers/requestController.js
+++ backend/src/controllers/requestController.js
@@ -15,8 +15,7 @@ exports.createRequest = (req, res) => {
     location,
     deadline
   };
-
-  // ✅ ADD THIS LINE
+  
   requests.push(newRequest);
 
   console.log("Saved Request:", newRequest);
@@ -30,5 +29,25 @@ exports.createRequest = (req, res) => {
 exports.getAllRequests = (req, res) => {
   res.json({
     data: requests
+  });
+};
+exports.deleteRequest = (req, res) => {
+  const id = parseInt(req.params.id);
+
+  // find index
+  const index = requests.findIndex(req => req.id === id);
+
+  if (index === -1) {
+    return res.status(404).json({
+      message: "Request not found"
+    });
+  }
+
+  // remove item
+  const deleted = requests.splice(index, 1);
+
+  res.json({
+    message: "Request deleted successfully",
+    data: deleted[0]
   });
 };
